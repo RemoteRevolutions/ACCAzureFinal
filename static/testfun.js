4ce1175999cf8e52a7c1a2a292087bbac1867644
Rich Hutnik (Remote Revolutions) <93293164+RemoteRevolutions@users.noreply.github.com>
2022-01-06 19:51:16 -0500

    Update testfun.js

--- static/testfun.js
+++ static/testfun.js
@@ -1,7 +1,7 @@
 function testfun(){
 const date = new Date(Date.now());
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let x = axios.post('https://remrevbuttonpush.azurewebsites.net', {
+let x = axios.post('https://prod-91.eastus.logic.azure.com:443/workflows/a130cc611cb649578cb5fcc5fe0dbdad/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5PlM1XqtNZ01tu_79erfmlvTYJY1Y8xKxPlnRQE8yPg', {
     id: self.crypto.randomUUID(),
     month: months[date.getMonth()],
     day: date.getDate(),
