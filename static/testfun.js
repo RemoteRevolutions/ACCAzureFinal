a05d7a1b7810c3a7b42cd2a2c6f6bdb3d23c5fdf
Rich Hutnik (Remote Revolutions) <93293164+RemoteRevolutions@users.noreply.github.com>
2022-01-22 18:46:34 -0500

    Update testfun.js
    yet another one, revert to broken URL

--- static/testfun.js
+++ static/testfun.js
@@ -1,7 +1,7 @@
 function testfun(){
 const date = new Date(Date.now());
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let x = axios.post('https://prod-21.eastus.logic.azure.com:443/workflows/2f223ecad61d4b9481e9cf79e7dac669/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MX5693MOOvraKjgG28rLF7yv4TAAGIOh3Q7qqWfwJXc', {
+let x = axios.post('https://remrevbuttonpush.azurewebsites.net', {
     id: self.crypto.randomUUID(),
     month: months[date.getMonth()],
     day: date.getDate(),
