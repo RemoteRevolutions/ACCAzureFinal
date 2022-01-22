a6b700455aeaaa1aa4f4b5b64e5df82ff2763c3f
Rich Hutnik (Remote Revolutions) <93293164+RemoteRevolutions@users.noreply.github.com>
2022-01-22 18:38:31 -0500

    Update testfun.js
    Fixed a typo in Logic App URL line

--- static/testfun.js
+++ static/testfun.js
@@ -1,7 +1,7 @@
 function testfun(){
 const date = new Date(Date.now());
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let x = axios.post('https://prod-68.eastus.logic.azure.com:443/workflows/5de3736d54c34adfa11b8cbc5fb3b1d9/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2QScxbkiwXFfGoY_wGqu0Sz-rBO1sr9h3hnHKvoLIIQ, {
+let x = axios.post('https://prod-68.eastus.logic.azure.com:443/workflows/5de3736d54c34adfa11b8cbc5fb3b1d9/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2QScxbkiwXFfGoY_wGqu0Sz-rBO1sr9h3hnHKvoLIIQ', {
     id: self.crypto.randomUUID(),
     month: months[date.getMonth()],
     day: date.getDate(),
