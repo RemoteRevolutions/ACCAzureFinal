921ced163c42f834d48a38caaabd58f6c837a0ca
Rich Hutnik (Remote Revolutions) <93293164+RemoteRevolutions@users.noreply.github.com>
2022-01-22 15:08:08 -0500

    Attempt to fix button not working

---
+++ templates/testfun.js
@@ -0,0 +1,19 @@
+function testfun(){
+const date = new Date(Date.now());
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let x = axios.post('https://prod-68.eastus.logic.azure.com:443/workflows/5de3736d54c34adfa11b8cbc5fb3b1d9/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2QScxbkiwXFfGoY_wGqu0Sz-rBO1sr9h3hnHKvoLIIQ
+', {
+    id: self.crypto.randomUUID(),
+    month: months[date.getMonth()],
+    day: date.getDate(),
+    year: date.getFullYear(),
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds()
+  })
+  .then(function (response) {
+    console.log(response);
+    return response.data
+  });}
+
+console.log(x);
