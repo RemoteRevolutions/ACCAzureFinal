15f7fe808f235e1cc8e7ed4cf20f0c14d8ad8f1b
Rich Hutnik (Remote Revolutions) <93293164+RemoteRevolutions@users.noreply.github.com>
2022-01-26 17:23:20 -0500

    Update testfun.js
    Update to refresh page.

--- templates/testfun.js
+++ templates/testfun.js
@@ -13,6 +13,10 @@ let x = axios.post('https://prod-21.eastus.logic.azure.com:443/workflows/2f223ec
   .then(function (response) {
     console.log(response);
     return response.data
-  });}
+  });
 
 console.log(x);
+window.location.reload();
+}
+
+
